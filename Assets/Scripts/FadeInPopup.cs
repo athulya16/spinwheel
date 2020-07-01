3756bb586fa48f2c2003baa2b90e68bfc9c2567a
Athulya <athulyababu16@gmail.com>
2020-07-02 00:37:00 +0530

    committing spinwheel

---
+++ Assets/Scripts/FadeInPopup.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInPopup : MonoBehaviour
+{
+    public Image popupBg;
+    public Button closeButton;
+    public SpinWheel spinWheelRef;
+    public Text winText;
+    public Text youWonText;
+    void Start()
+    {
+        popupBg.canvasRenderer.SetAlpha(0.0f);
+    }
+
+    public void FadeIn(float win)
+    {
+        winText.text = win.ToString();
+        popupBg.CrossFadeAlpha(1, 1, false);
+        Invoke("ShowYouWonText", 1);
+    }
+
+    void ShowYouWonText()
+    {
+        youWonText.gameObject.SetActive(true);
+        Invoke("ShowWinText", 0.5f);
+    }
+    
+    void ShowWinText()
+    {
+        winText.gameObject.SetActive(true);
+        ShowCloseButton();
+    }
+
+    void ShowCloseButton()
+    {
+        closeButton.gameObject.SetActive(true);
+        closeButton.interactable = true;
+    }
+
+    public void ClosePopup()
+    {
+        popupBg.CrossFadeAlpha(0, 1, false);
+        closeButton.gameObject.SetActive(false);
+        winText.gameObject.SetActive(false);
+        youWonText.gameObject.SetActive(false);
+        spinWheelRef.GetComponent<SpinWheel>().OnWinPopupClosed();
+    }
+}
---
+++ Assets/Scripts/SpinWheel.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class SpinWheel : MonoBehaviour
+{
+    public Text[] winTextArray;
+    public Button spinButton;
+    public UIParticleSystem emitter;
+    public Image WinPopup;
+    private bool isStarted;
+    private float[] sectorAngles;
+    private float finalAngle;
+    private float startAngle = 0;
+    private float currentLerpRotationTime;
+    private int selectedWinIndex = -1;
+    private float sectorAngle = 30;
+    bool isSpinning = false;
+    float spinWin = 0;
+    void Start()
+    {
+        SetWinValues();
+    }
+
+    void SetWinValues()
+    {
+        for(int i = 0; i< winTextArray.Length; i++)
+        {
+            winTextArray[i].transform.rotation = Quaternion.Euler(0, 0 , sectorAngle * -i);
+            winTextArray[i].text =  ((i+1)*sectorAngle).ToString();
+        }
+    }
+
+    void Update()
+    {
+        if (isStarted)
+        {
+            isSpinning = true;
+            spinButton.interactable = false;
+        }
+        else
+        {
+            if(isSpinning)
+            {
+                Invoke("OnSpinCompleted", 1);
+                isSpinning = false;
+            }
+        }
+
+        if (!isStarted)
+            return;
+
+        float maxLerpRotationTime = 4f;
+        currentLerpRotationTime += Time.deltaTime;
+
+        if (currentLerpRotationTime > maxLerpRotationTime || this.transform.eulerAngles.z == finalAngle)
+        {
+            currentLerpRotationTime = maxLerpRotationTime;
+            isStarted = false;
+            startAngle = finalAngle % 360;
+        }
+
+        float t = currentLerpRotationTime / maxLerpRotationTime;
+        t = t * t * t * (t * (6f * t - 15f) + 10f);
+        float angle = Mathf.Lerp(startAngle, finalAngle, t);
+        this.transform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    public void TurnWheel()
+    {
+        currentLerpRotationTime = 0f;
+        sectorAngles = new float[] { 360, 330, 300, 270, 240, 210, 180, 150, 120, 90, 60, 30};
+        int fullCircles = 5;
+        selectedWinIndex = UnityEngine.Random.Range(0, sectorAngles.Length);
+        float randomFinalAngle = sectorAngles[selectedWinIndex];
+        finalAngle = -(fullCircles * 360 + randomFinalAngle);
+        isStarted = true;
+    }
+
+    void OnSpinCompleted()
+    {
+        spinWin = (selectedWinIndex + 1) * sectorAngle;
+        WinPopup.GetComponent<FadeInPopup>().FadeIn(spinWin);
+        emitter.Play();
+    }
+
+    public void OnWinPopupClosed()
+    {
+        emitter.Stop();
+        Invoke("EnableSpinButton", 1);
+    }
+
+    void EnableSpinButton()
+    {
+        spinButton.interactable = true;
+    }
+}
